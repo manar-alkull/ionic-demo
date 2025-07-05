abc6400534abc7aa61a2e97e902c9b9b12382956
manar.alkul <manar.alkul@connamix.com>
2025-07-05 05:41:06 +0300

    PhotoService (works)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { PhotoService } from '../services/photo.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton,
+     IonIcon, IonGrid, IonRow, IonCol, IonImg, CommonModule]
 })
 export class Tab2Page {
 
+  photoService :PhotoService=inject(PhotoService);
   constructor() {}
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,19 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import { addIcons } from 'ionicons';
+import { camera, home, image, images, person } from 'ionicons/icons';
+
+defineCustomElements(window);
+
+addIcons({
+  'image': image,
+  'images': images,
+  'home': home,
+  'person': person,
+  'camera':camera
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
